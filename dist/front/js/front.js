326aff37704e26b8d3b38083b648871fe4e7fff5
kiminkyung <kik87827@gmail.com>
2022-07-24 11:53:03 +0900

    추가7

--- dist/front/js/front.js
+++ dist/front/js/front.js
@@ -6,6 +6,7 @@ var userAgent=navigator.userAgent.toLowerCase();
 ;(function($){$.browserTest=function(a,z){var u='unknown',x='X',m=function(r,h){for(var i=0;i<h.length;i=i+1){r=r.replace(h[i][0],h[i][1]);}return r;},c=function(i,a,b,c){var r={name:m((a.exec(i)||[u,u])[1],b)};r[r.name]=true;r.version=(c.exec(i)||[x,x,x,x])[3];if(r.name.match(/safari/)&&r.version>400){r.version='2.0';}if(r.name==='presto'){r.version=($.browser.version>9.27)?'futhark':'linear_b';}r.versionNumber=parseFloat(r.version,10)||0;r.versionX=(r.version!==x)?(r.version+'').substr(0,1):x;r.className=r.name+r.versionX;return r;};a=(a.match(/Opera|Navigator|Minefield|KHTML|Chrome/)?m(a,[[/(Firefox|MSIE|KHTML,\slike\sGecko|Konqueror)/,''],['Chrome Safari','Chrome'],['KHTML','Konqueror'],['Minefield','Firefox'],['Navigator','Netscape']]):a).toLowerCase();$.browser=$.extend((!z)?$.browser:{},c(a,/(camino|chrome|firefox|netscape|konqueror|lynx|msie|opera|safari)/,[],/(camino|chrome|firefox|netscape|netscape6|opera|version|konqueror|lynx|msie|safari)(\/|\s)([a-z0-9\.\+]*?)(\;|dev|rel|\s|$)/));$.layout=c(a,/(gecko|konqueror|msie|opera|webkit)/,[['konqueror','khtml'],['msie','trident'],['opera','presto']],/(applewebkit|rv|konqueror|msie)(\:|\/|\s)([a-z0-9\.]*?)(\;|\)|\s)/);$.os={name:(/(win|mac|linux|sunos|solaris|iphone)/.exec(navigator.platform.toLowerCase())||[u])[0].replace('sunos','solaris')};if(!z){$('html').addClass([$.os.name,$.browser.name,$.browser.className,$.layout.name,$.layout.className].join(' '));}};$.browserTest(navigator.userAgent);})(jQuery);//http://jquery.thewikies.com/browser/
 $(function(){
 	subvisualFunc();
+	calendarFunc();
 });
 $(window).on("load",function(){
 	headerFunc();
@@ -20,6 +21,24 @@ function menurock(target){
 	});
 }
 
+function calendarFunc(){
+	var $datepicker = $(".calendar_call");
+	if($datepicker.length){
+		$datepicker.datepicker({
+			monthNamesShort: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+			dayNamesMin: ["일", "월", "화", "수", "목", "금", "토"],
+			changeMonth: true,
+			changeYear: true,
+			dateFormat: 'yy-mm-dd'
+		});
+		var $windowWidth = 0;
+		$(window).on("resize",function(){
+			if($windowWidth == $(window).width() && touchstart){return;}
+			$datepicker.datepicker("hide");
+			$windowWidth = $(window).width();
+		});
+	}
+}
 
 function headerFunc(){
 	var $htop_gnb_list_w = $(".nav_wrap");
